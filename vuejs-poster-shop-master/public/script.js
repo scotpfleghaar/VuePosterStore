9a4c45d86f6f087999f98dc6d01e1c9ab050fa06
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-04-30 07:49:21 -0500

    changes

--- vuejs-poster-shop-master/public/script.js
+++ vuejs-poster-shop-master/public/script.js
@@ -1,1 +1,29 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [{
+                title: 'item 1'
+            },
+            {
+                title: 'item 2'
+            },
+            {
+                title: 'item 3'
+            }
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function (index) {
+            this.total += 9.99;
+            this.total = Math.round(this.total * 100) / 100;
+
+            var item = this.items[index];
+            this.cart.push({
+                title: item.title,
+                qty: 1,
+            });
+        }
+    }
+});
