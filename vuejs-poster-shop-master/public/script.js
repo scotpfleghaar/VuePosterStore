c2d879a7ec99f3377faa4ad1e74cd8a7c9d1d8df
Scot Pfleghaar <scot.pfleghaar@gmail.com>
2018-05-08 07:30:28 -0500

    fixes

--- vuejs-poster-shop-master/public/script.js
+++ vuejs-poster-shop-master/public/script.js
@@ -1,29 +1,59 @@
+var PRICE = 9.99;
 new Vue({
     el: '#app',
     data: {
         total: 0,
-        items: [{
-                title: 'item 1'
-            },
-            {
-                title: 'item 2'
-            },
-            {
-                title: 'item 3'
-            }
-        ],
-        cart: []
+        items: [],
+        cart: [],
+        search: "",
     },
     methods: {
+        onSubmit: function(){
+           this.$http
+           .get('/search/'.concat(this.search))
+           .then(function(res){
+                this.items = res.data;
+           });
+        },
         addItem: function (index) {
             this.total += 9.99;
-            this.total = Math.round(this.total * 100) / 100;
 
             var item = this.items[index];
-            this.cart.push({
-                title: item.title,
-                qty: 1,
-            });
+            var found = false;
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: PRICE,
+                });
+            }
+        },
+        inc: function (item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec: function (item) {
+            item.qty--;
+            this.total -= PRICE;
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    this.cart.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function (price) {
+            return "$".concat(price.toFixed(2));
         }
     }
 });
